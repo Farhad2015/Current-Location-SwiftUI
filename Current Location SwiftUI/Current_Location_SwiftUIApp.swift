bf5f70b9cd7b193bab1023b5e81e12f39cd4004f
Mahmudur Rahman <mahmud@Mahmudurs-Mac-mini.local>
2022-04-06 12:36:14 +0600

    First Commit

---
+++ Current Location SwiftUI/Current_Location_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Current_Location_SwiftUIApp.swift
+//  Current Location SwiftUI
+//
+//  Created by Mahmudur Rahman on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Current_Location_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
